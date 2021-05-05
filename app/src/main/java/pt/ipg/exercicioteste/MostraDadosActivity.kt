cb7810a64483f547ed1fb204be08ee296d725ef5
Rafael Santos <rdsrafael@gmail.com>
2021-05-05 09:40:05 +0100

    Initial commit

---
+++ app/src/main/java/pt/ipg/exercicioteste/MostraDadosActivity.kt
@@ -0,0 +1,11 @@
+package pt.ipg.exercicioteste
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MostraDadosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mostra_dados)
+    }
+}
